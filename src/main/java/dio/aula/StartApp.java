81fb9f8f39ab383f320a369692bb1a4b48f81fc9
jeremiasfreitass <jeremiasjmf@hotmail.com>
2023-01-23 21:39:08 -0300

    Iniciando projeto

---
+++ src/main/java/dio/aula/StartApp.java
@@ -0,0 +1,26 @@
+package dio.aula;
+
+import dio.aula.model.User;
+import dio.aula.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class StartApp implements CommandLineRunner {
+    @Autowired
+    private UserRepository repository;
+    @Override
+    public void run(String... args) throws Exception {
+        User user = new User();
+        user.setName("Jeremias");
+        user.setUserName("jerefreitas");
+        user.setPassword("123456");
+
+        repository.save(user);
+
+        for (User u : repository.findAll()){
+            System.out.println(u);
+        }
+    }
+}
